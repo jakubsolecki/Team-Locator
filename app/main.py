a578e30b6bb2a8310fe9dca1d6ad76ccd35e5953
Jakub <jakub.solecki@onet.com.pl>
2020-04-12 15:20:26 +0200

    fighitng with android environment

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 import kivy
 from kivy.app import App
-from geopy.geocoders import Nominatim
+# from geopy.geocoders import Nominatim
 from kivy.garden.mapview import MapView, MapMarker
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
+import android
+
+
+droid = android.Android()
 
 
 class WindowManager(ScreenManager):
@@ -16,8 +20,12 @@ class TokenWindow(Screen):
 
 
 class MapWindow(Screen):
-    geolocator = Nominatim(user_agent="GeoTestApp")
-    location = geolocator.geocode("21 Kawiory, Kraków")
+    # geolocator = Nominatim(user_agent="GeoTestApp")
+    # location = geolocator.geocode("21 Kawiory, Kraków")
+    location = droid.getLastKnownLocation().result
+    location = location.get('gps') or location.get('network')
+    lon = droid.geocode(location['longitude'])
+    lat = droid.geocode(location['latitude'])
     pass
 
 
