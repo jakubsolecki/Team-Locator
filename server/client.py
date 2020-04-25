57ca64690cf6cf1541c726e760022a9f11ee2c23
Jakub Solecki <jakub.solecki@onet.com.pl>
2020-04-25 19:44:14 +0200

    working on target functionality

--- server/client.py
+++ server/client.py
@@ -1,43 +1,76 @@
 import socket
 import pickle
+import threading
+import time
 
 
 HEADER_SIZE = 64
 PORT = 5050
-SERVER = "127.0.1.1"  # different when the client doesn't run on the same machine (check eg. IP list on router )
+SERVER = socket.gethostbyname(socket.gethostname())  # TODO: provide from user input
 ADDRESS = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
+REQUEST_LOCATIONS = "!REQUEST_LOCATIONS"
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDRESS)
+# client.connect(ADDRESS)
+
+
+# connect to server using it's ip <- will be provided form user input
+def connect(server_ip):
+    client.connect((server_ip, PORT))
+    name = "Jan Kowalski"  # TODO: provide from user input
+    # update_location_thread = threading.Thread(target=update_location, args=name)
+    # update_location_thread.start()  # start updating current location
+    receive_messages_thread = threading.Thread(target=receive)
+    receive_messages_thread.start()  # start listening to the server messages
+
+
+def update_location(name):
+    while True:
+        time.sleep(10)
+        # TODO: reading location from gps
+        lon = 51.6363
+        lat = 51.6363
+        data_to_send = pickle.dumps((name, lon, lat))
+        send(data_to_send)
 
 
 def send(msg):
-    # message = msg.encode(FORMAT)
-    # msg_length = len(message)
-    # send_length = str(msg_length).encode(FORMAT)
-    # send_length += b' ' * (HEADER_SIZE - len(send_length))
-    # client.send(send_length)
-    # client.send(message)
-    # print(client.recv(2048).decode(FORMAT))  # TODO: buffer data
     message = pickle.dumps(msg)
     msg_length = len(message)
     send_length = str(msg_length).encode(FORMAT)
     send_length += b' ' * (HEADER_SIZE - len(send_length))
     client.send(send_length)
     client.send(message)
-    # print(client.recv(2048).decode(FORMAT))
-    print(pickle.loads(client.recv(1024)))
+    # rep = pickle.loads(client.recv(2048))
+    # print(rep)
 
 
+def receive():
+    while True:
+        msg_length = client.recv(HEADER_SIZE).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = pickle.loads(client.recv(msg_length))
+            print(msg)
+            # if isinstance(msg, list):
+            #     print(msg)
+            #     #  update list of teammates locations
+            # elif msg == "Message received":
+            #     print(msg)
+
+
+connect(SERVER)
 send("Hello world!")
 input()
 send("Hello everyone!")
 input()
 send("Hello!")
 input()
-data = {"ID": 1, "name": "Jakub Solecki", "lon": 50.458673, "lat": 51.906735}
-# data_to_send = pickle.dumps(data)
+data = ("Jakub Solecki", 50.458673, 51.906735)
 send(data)
+input()
+send(REQUEST_LOCATIONS)
+input()
 send(DISCONNECT_MESSAGE)
--- server/server.py
+++ server/server.py
@@ -1,49 +1,98 @@
 import socket
 import threading
 import pickle
+import sys
 
 
 HEADER_SIZE = 64
 PORT = 5050
-# SERVER = socket.gethostbyname(socket.gethostname())  # If it is not used from localhost it can't be defined
-ADDRESS = ('', PORT)
+SERVER = socket.gethostbyname(socket.gethostname())  # + ".local" so that it's no longer localhost-only
+ADDRESS = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
+REQUEST_LOCATIONS = "!REQUEST_LOCATIONS"
+
+clients = []  # TODO: define maximum number of clients (needs thread testing)
+clients_data = []  # list of tuples: (connection (IP), name, lon, lat)
 
 # TODO: objectify
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDRESS)
+print("Socket created successfully")
+try:
+    server.bind(ADDRESS)
+except socket.error as errmsg:
+    print("Bind failed. Error code: " + errmsg[0] + "\nMessage: " + errmsg[1])
+    sys.exit()
+print("Socket bind complete")
 
 
 #  handle connected client
 def handle_client(connection, address):
     print(f"[NEW CONNECTION] {address} connected")
-
     connected = True
+    lock = threading.RLock()
+    global clients_data
+
     while connected:
         msg_length = connection.recv(HEADER_SIZE).decode(FORMAT)
         if msg_length:
             msg_length = int(msg_length)
-            # msg = connection.recv(msg_length).decode(FORMAT)
             msg = pickle.loads(connection.recv(msg_length))
+            reply = "Message received!"
             if msg == DISCONNECT_MESSAGE:
                 connected = False
+            elif isinstance(msg, tuple):
+                with lock:
+                    tmp = (connection, msg[0], msg[1], msg[2])
+                    if tmp not in clients_data:
+                        clients_data.append(tmp)
+                    clients_data = [msg if data[0] == msg[0] else data for data in clients_data]  # update client location
+            elif msg == REQUEST_LOCATIONS:
+                with lock:
+                    data = clients_data[:]
+                reply = [(tup[1], tup[2], tup[3]) for tup in data]
             print(f"[{address}] {msg}")
+            reply = pickle.dumps(reply)
+            rpl_length = len(reply)
+            send_length = str(rpl_length).encode(FORMAT)
+            send_length += b' ' * (HEADER_SIZE - len(send_length))
+            connection.send(send_length)
+            connection.send(reply)
             # connection.send("Message received".encode(FORMAT))
-            connection.send(pickle.dumps(msg))
+            # connection.send(pickle.dumps(msg))
 
+    with lock:
+        clients.remove((connection, address))
     connection.close()
+
+
+def handle_new_connections():
+    while True:
+        connection, address = server.accept()
+        clients.append((connection, address))
+        thread = threading.Thread(target=handle_client, args=(connection, address))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}\n")
+
+
+# def stop_server():
+#     while True:
+#         inpt = input()
+#         if inpt == "QUIT":
+#             if clients:
+#                 for client in clients:
+#                     connection = client[0]
+#                     connection.close
+#             sys.exit()
 
 
 # handle new connections
 def start():
     server.listen()
-    # print(f"[LISTENING] Server is listening {SERVER}")
-    while True:
-        connection, address = server.accept()
-        thread = threading.Thread(target=handle_client, args=(connection, address))
-        thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}\n")
+    # thread = threading.Thread(target=stop_server)
+    # thread.start()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    handle_new_connections()
 
 
 print("[STARTING] server is starting...")
