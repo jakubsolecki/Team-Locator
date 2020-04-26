e7269f4dd3893a4fcf6d235114223ffa29c49ccc
Jakub Solecki <jakub.solecki@onet.com.pl>
2020-04-26 13:55:03 +0200

    implementing separate teams (tokens)

--- server/client.py
+++ server/client.py
@@ -2,13 +2,14 @@ import socket
 import pickle
 import threading
 import time
+import sys
 
 '''
 General TODO:
     - setup messages:
         1) provide server ip from user input (glorious "connect" on app's main screen)
-        2) provide token from user input (separate token for each team), then send it to the server in INIT message
-        3) provide name from user input (see above)
+        2) provide token and username from user input (separate token for each team), then send it to the server in INIT 
+        message
     - reading current gps location (maybe providing it from outside -> change update_location(name))     
     - updating teammapview's list of teammates locations (in receive() -> when message is a list)
 '''
@@ -16,11 +17,13 @@ General TODO:
 # TODO: move to the class?
 HEADER_SIZE = 64
 PORT = 5050
-SERVER = socket.gethostbyname(socket.gethostname())  # TODO: provide from user input (server ipv4 so far)
+SERVER = socket.gethostbyname(socket.gethostname())  # TODO: provide from user input (server IPv4 so far)
 ADDRESS = (SERVER, PORT)
 FORMAT = 'utf-8'
+INIT_MESSAGE = "!INIT"
 DISCONNECT_MESSAGE = "!DISCONNECT"
 REQUEST_LOCATIONS = "!REQUEST_LOCATIONS"
+UPDATE_LOCATION = "!UPDATE_LOCATION"
 
 
 # ====================== Client is a singleton so it must always be created using get_instance() ======================
@@ -39,41 +42,50 @@ class Client:
             raise Exception("Client class must be a singleton!")
         else:
             Client.__instance = self
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._token = None
+        self._name = None
 
     # connect to server using it's ip <- will be provided form user input
     def connect(self, server_ip):
-        self.client.connect((server_ip, PORT))
+        self._client.connect((server_ip, PORT))
         name = "Jan Kowalski"  # TODO: provide from user input
-        # update_location_thread = threading.Thread(target=update_location, args=name)
+        # update_location_thread = threading.Thread(target=_update_location, args=name)
         # update_location_thread.start()  # start updating current location
-        receive_messages_thread = threading.Thread(target=self.receive)
+        receive_messages_thread = threading.Thread(target=self._receive_message)
+        receive_messages_thread.daemon = True  # do I need this?
         receive_messages_thread.start()  # start listening to the server messages
 
+    def send_message_via_client(self, msg, msg_type):
+        if msg_type == INIT_MESSAGE:
+            self._token, self._name = msg.split(':', 1)
+        msg = (msg_type, msg)
+        self._send_message(msg)
+
     # send current location to server every 10 seconds
-    def update_location(self, name):
+    def _update_location(self, name):
         while True:
             time.sleep(10)
             # TODO: reading location from gps
             lon = 51.6363
             lat = 51.6363
-            data_to_send = pickle.dumps((name, lon, lat))
-            self.send(data_to_send)
+            data_to_send = pickle.dumps((UPDATE_LOCATION, (self._token, (name, lon, lat))))
+            self._send_message(data_to_send)
 
-    def send(self, msg):
+    def _send_message(self, msg):
         message = pickle.dumps(msg)
         msg_length = len(message)
         send_length = str(msg_length).encode(FORMAT)
         send_length += b' ' * (HEADER_SIZE - len(send_length))
-        self.client.send(send_length)  # first sending length
-        self.client.send(message)  # then actual message
+        self._client.send(send_length)  # first sending length
+        self._client.send(message)  # then actual message
 
-    def receive(self):
+    def _receive_message(self):
         while True:
-            msg_length = self.client.recv(HEADER_SIZE).decode(FORMAT)
+            msg_length = self._client.recv(HEADER_SIZE).decode(FORMAT)
             if msg_length:
                 msg_length = int(msg_length)
-                msg = pickle.loads(self.client.recv(msg_length))
+                msg = pickle.loads(self._client.recv(msg_length))
                 print(msg)
                 # TODO: handling received messages according to their content
                 # if isinstance(msg, list):
@@ -86,15 +98,17 @@ class Client:
 # hardcoded testing
 x = Client().get_instance()
 x.connect(SERVER)
-x.send("Hello world!")
+x.send_message_via_client("#ABCD:Jakub Solecki", INIT_MESSAGE)
 input()
-x.send("Hello everyone!")
+x.send_message_via_client("Hello world!", "TEST")
 input()
-x.send("Hello!")
+x.send_message_via_client(None, REQUEST_LOCATIONS)
 input()
 data = ("Jakub Solecki", 50.458673, 51.906735)
-x.send(data)
+x.send_message_via_client(("#ABCD", data), UPDATE_LOCATION)
 input()
-x.send(REQUEST_LOCATIONS)
+x.send_message_via_client("#ABCD", REQUEST_LOCATIONS)
 input()
-x.send(DISCONNECT_MESSAGE)
+x.send_message_via_client(None, DISCONNECT_MESSAGE)
+input()
+sys.exit()
--- server/server.py
+++ server/server.py
@@ -3,6 +3,8 @@ import socket
 import threading
 import pickle
 import sys
+from random import choice
+from string import ascii_uppercase
 
 '''
 So far server works as long as all devices are within same wifi. Global version coming soon.
@@ -19,22 +21,26 @@ PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())  # + ".local" so that it's no longer localhost-only
 ADDRESS = (SERVER, PORT)
 FORMAT = 'utf-8'
+INIT_MESSAGE = "!INIT"
 DISCONNECT_MESSAGE = "!DISCONNECT"
 REQUEST_LOCATIONS = "!REQUEST_LOCATIONS"
+UPDATE_LOCATION = "!UPDATE_LOCATION"
 
 
 class Server:
     clients = []  # connected clients TODO: (or not) define maximum number of clients (to be determined after testing)
     clients_data = []  # list of tuples: (connection (IP), name, lon, lat)
+    tokens = []
+    client_locations = {}
 
     def __init__(self):
         print("[STARTING] server is starting...")
         self.exit_request_flag = threading.Event()
-        signal.signal(signal.SIGINT, self.sigint_handler)
+        signal.signal(signal.SIGINT, self._sigint_handler)
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         print("Socket created successfully")
 
-    def sigint_handler(self, signum, stack_frame):
+    def _sigint_handler(self, signum, stack_frame):
         self.stop_server()
 
     # server abort
@@ -55,42 +61,73 @@ class Server:
         print("Socket bind complete")
         self.server.listen()
         print(f"[LISTENING] Server is listening on {SERVER}")
-        self.handle_new_connections()
+        handle_new_connections_thread = threading.Thread(target=self._handle_new_connections)
+        handle_new_connections_thread.daemon = True
+        handle_new_connections_thread.start()
+        # self._handle_new_connections()
 
     #  handle connected client
-    def handle_client(self, connection, address):
+    def _handle_client(self, connection, address):
         print(f"[NEW CONNECTION] {address} connected")
         connected = True
         lock = threading.RLock()
 
         while connected:
             msg_length = connection.recv(HEADER_SIZE).decode(FORMAT)
+            
             if msg_length:
                 msg_length = int(msg_length)
                 msg = pickle.loads(connection.recv(msg_length))
                 reply = "Message received!"  # TODO: remove in final version (debug purposes only)
-                if msg == DISCONNECT_MESSAGE:
+
+                if msg[0] == DISCONNECT_MESSAGE:
                     connected = False
-                elif isinstance(msg, tuple):
+                elif msg[0] == UPDATE_LOCATION:
                     with lock:
-                        tmp = (connection, msg[0], msg[1], msg[2])
-                        if tmp not in self.clients_data:
-                            self.clients_data.append(tmp)
-                        # update client's location
-                        self.clients_data = [msg if data[0] == msg[0] else data for data in self.clients_data]
-                elif msg == REQUEST_LOCATIONS:
+                        self.client_locations[(msg[1][0], connection)] = msg[1][1]
+                elif msg[0] == REQUEST_LOCATIONS:
                     with lock:
-                        data = self.clients_data[:]
-                    reply = [(tup[1], tup[2], tup[3]) for tup in data]
+                        locations = []
+                        for key in self.client_locations.keys():
+                            if key[0] == msg[1]:
+                                locations.append(self.client_locations[key])
+                        reply = locations
+                elif msg[0] == INIT_MESSAGE:
+                    token, name = msg[1].split(':', 1)
+                    with lock:
+                        if token in self.tokens:
+                            self.client_locations.update({(token, connection): (name, -1, -1)})
+                        else:
+                            print("Incorrect token")
+
                 print(f"[{address}] {msg}")
-                self.send(connection, reply)
+                self._send_message(connection, reply)
 
         with lock:
             self.clients.remove((connection, address))
         connection.close()
 
+    # def _read_message(self, msg):
+    #     lock = threading.RLock()
+    #     reply = "Message received!"
+    #     if msg == DISCONNECT_MESSAGE:
+    #         connected = False
+    #     elif isinstance(msg, tuple):
+    #         with lock:
+    #             tmp = (connection, msg[0], msg[1], msg[2])
+    #             if tmp not in self.clients_data:
+    #                 self.clients_data.append(tmp)
+    #             # update client's location
+    #             self.clients_data = [msg if data[0] == msg[0] else data for data in self.clients_data]
+    #     elif msg == REQUEST_LOCATIONS:
+    #         with lock:
+    #             data = self.clients_data[:]
+    #         reply = [(tup[1], tup[2], tup[3]) for tup in data]
+    #     print(f"[{address}] {msg}")
+    #     self._send_message(connection, reply)
+
     # send message to client
-    def send(self, connection, msg):
+    def _send_message(self, connection, msg):
         msg = pickle.dumps(msg)
         length = len(msg)
         msg_length = str(length).encode(FORMAT)
@@ -99,20 +136,34 @@ class Server:
         connection.send(msg)         # then actual message
 
     # listen and connect new clients
-    def handle_new_connections(self):
+    def _handle_new_connections(self):
         while True:
             connection, address = self.server.accept()
             self.clients.append((connection, address))
-            handle_new_client_thread = threading.Thread(target=self.handle_client, args=(connection, address))
+            handle_new_client_thread = threading.Thread(target=self._handle_client, args=(connection, address))
             try:
                 handle_new_client_thread.daemon = True  # all spawned threads exist as long as the main thread does
                 handle_new_client_thread.start()  # handle communication with each client in a separate thread
             except (KeyboardInterrupt, SystemExit, signal.SIGINT):
                 self.stop_server()
 
-            print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}\n")
+            print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 2}\n")
+
+    def generate_token(self):
+        # token = ''.join(choice(ascii_uppercase) for i in range(12))
+        #
+        # while token in self.tokens:
+        #     token = ''.join(choice(ascii_uppercase) for i in range(12))
+
+        token = "#ABCD"
+        self.tokens.append(token)
+        print(f"[NEW TOKEN] {token}")
 
 
 # hardcoded testing
 s = Server()
+s.generate_token()
+# s.generate_token()
+# s.generate_token()
 s.start()
+input("Press enter to exit")
